8fcc958e7a4c93ae86c29e1982e28da40e410e68
Danilo Pereira <danilo_tw@hotmail.com>
2023-10-16 21:45:23 +0100

    initial commit

---
+++ management-service/src/test/java/com/babyfeed/managementservice/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyfeed.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
