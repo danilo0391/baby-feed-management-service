afaa9c8398c2d4e7fc80dcc3db63086686103657
Danilo Pereira <danilo_tw@hotmail.com>
2023-10-30 18:10:07 +0000

    protection test

--- src/main/java/com/babyfeed/managementservice/controller/RegisterItemController.java
+++ src/main/java/com/babyfeed/managementservice/controller/RegisterItemController.java
@@ -10,11 +10,11 @@ public class RegisterItemController {
 
     @RequestMapping("/items")
     public String[] getAllItems(){
-        return new String[]{"Fralda", "Coco", "Feed", "120ml leite"};
+        return new String[]{"Fralda", "Coco", "Formula", "120ml"};
     }
 
     @RequestMapping("/items/id")
     public String[] getItemById(){
-        return new String[]{"Feed", "120ml leite"};
+        return new String[]{"Formula", "120ml"};
     }
 }
