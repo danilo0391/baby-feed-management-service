df69d6ee7b84b1d3ecfe78e477b6db151dcddcfa
Danilo Pereira <danilo_tw@hotmail.com>
2023-11-06 21:43:14 +0000

    updates

--- src/main/java/com/babyfeed/managementservice/model/Baby.java
+++ src/main/java/com/babyfeed/managementservice/model/Baby.java
@@ -1,12 +1,17 @@
 package com.babyfeed.managementservice.model;
 
 import com.babyfeed.managementservice.enums.Gender;
+import jakarta.annotation.Generated;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenerationTime;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Table(name = "babies")
@@ -19,9 +24,13 @@ public class Baby {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private Long id;
+    @NotNull
     private String name;
-    private Date dateOfBirth;
+    @NotNull
+    private LocalDateTime dateOfBirth;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Gender gender;
 }
